417af72304e162e944471f767349b20a2239e787
Zoe Meng <zoebo1225@gmail.com>
2021-05-24 20:33:22 -0700

    completed structure and styling on itemslist page. sorting function and Google Map are work from the fake data.

---
+++ src/components/ItemList/Item/Item.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card, Row, Col } from 'antd';
+
+const {Meta} = Card;
+
+
+const Item = (props) => {
+  const {Products, changeData} = props;
+  return(
+    <Row>
+    {
+      Products?.map(item => (
+        <Col span = {8}>
+          <Card 
+            hoverable
+            onClick = {() => changeData(item)}
+            key = {item?.id}
+            style = {{width : 250, marginBottom: 50}}
+            cover = {<img alt="item" src= {item?.image} />}
+            >
+           <Meta title={item?.title} description= {`$${item?.price}`}/>
+          </Card>
+       </Col>      
+      ))
+    }
+    </Row>
+  )
+}
+
+export default Item;
---
+++ src/components/ItemList/Map/GoogleMap.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import { Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { GOOGLE_API_KEY } from '../../../constants/constants';
+import Avatar from '../../../assets/images/user.svg';
+
+const style = {
+  width: '100%',
+  height: '90vh'
+}
+
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      showingInfoWindow: true,
+      activeMarker: {},
+      selectedPlace: {},
+  
+      mapCenter: {
+        lat: 37.773972,
+        lng: -122.431297
+      }
+    }; 
+  }
+
+
+  render() {
+    return (
+      <Map 
+        google={this.props.google} 
+        initialCenter={{
+          lat: this.state.mapCenter.lat,
+          lng: this.state.mapCenter.lng  
+        }}
+        center={{
+            lat: this.state.mapCenter.lat,
+            lng: this.state.mapCenter.lng
+        }}
+        containerStyle={style}
+        zoom = {12}
+      >
+        <Marker 
+          position={{
+            lat: this.state.mapCenter.lat,
+            lng: this.state.mapCenter.lng
+          }}
+          icon={{
+            url: Avatar, 
+            scaledSize: this.props.google.maps.Size(5,5)
+          }}
+          
+        />
+        <Marker 
+          position={{
+            lat: this.props.latitude,
+            lng: this.props.longitude
+          }}
+          onClick = {() => this.onMarkerClick()}
+        >
+        </Marker>
+      </Map>
+    );
+  }
+}
+  
+
+
+export default GoogleApiWrapper({
+  apiKey: (GOOGLE_API_KEY)
+})(MapContainer)
