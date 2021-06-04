3ede9e52a50f1a1dd23f8859da1013108f606ed6
tommyshang <nshang@ucdavis.edu>
2021-06-04 20:26:18 +0800

    Implement Listing Detail Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-<<<<<<< HEAD
-import HomePage from "./components/Home/Home";
-import ItemList from "./components/ItemList/ItemList";
-import MyListings from "./components/MyListings/MyListings";
-import SavedListings from "./components/SavedListings/SavedListings";
-import ListingDetail from "./components/ListingDetail/ListingDetail";
-
-import "./App.css";
-=======
 import HomePage from './components/Home/Home';
 import ItemList from './components/ItemList/ItemList';
 import MyListings from './components/MyListings/MyListings';
 import SavedListings from './components/SavedListings/SavedListings';
 import './App.css';
->>>>>>> 6faec088950f662409bc0a50d0e459dbd2e033b5
-
+import ListingDetail from "./components/ListingDetail/ListingDetail";
 // this component should be a root router file
 function App() {
   return (
@@ -25,14 +15,9 @@ function App() {
         {/* add your router here like this. */}
         <Route exact path="/" component={HomePage} />
         <Route path="/items" component={ItemList} />
-<<<<<<< HEAD
-        <Route path="/my-listings" component={MyListings} />
-        <Route path="/saved-listings" component={SavedListings} />
-        <Route path="/listing-detail" component={ListingDetail} />
-=======
         <Route exact path="/my-listings-page" component={MyListings} />
         <Route exact path="/saved-listings-page" component={SavedListings} />
->>>>>>> 6faec088950f662409bc0a50d0e459dbd2e033b5
+        <Route path="/listing-detail" component={ListingDetail} />
       </Switch>
     </BrowserRouter>
   );
