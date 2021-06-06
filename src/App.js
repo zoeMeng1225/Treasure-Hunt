df0acf7a2dab138ab5676240dcc69e69a8af358a
Chan Rao <lichengrao@gmail.com>
2021-06-06 02:04:58 -0400

    Revert file name changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import MyListings from './components/MyListings/MyListings';
 import SavedListings from './components/SavedListings/SavedListings';
 
 import Login from 'components/Authentication/Login/Login';
-import Signup from 'components/Authentication/Signup/Signup';
+import SignUp from 'components/Authentication/SignUp/SignUp';
 import ListingDetail from './components/ListingDetail/ListingDetail';
 
 import './App.css';
@@ -19,7 +19,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/items" component={ItemList} />
         <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        <Route path="/signup" component={SignUp} />
         <Route exact path="/my-listings" component={MyListings} />
         <Route exact path="/saved-listings" component={SavedListings} />
         <Route path="/listing-detail" component={ListingDetail} />
