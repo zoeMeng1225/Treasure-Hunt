b6be3dd50e38cd6a338a8ecf0788eab47d8d52bf
Fangqing Xia <fangqingxia1994@tamu.edu>
2021-06-06 01:52:46 -0500

    implement use-search hook

--- src/hooks/use-search/index.js
+++ src/hooks/use-search/index.js
@@ -1,43 +1,12 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-// const parameters = {
-//     category : undefined,
-//     keyword : 'phone',
-//     latitude : '70.01',
-//     longititude : '-40.00',
-//  }
-
-// const parameters = {
-//     category : 'Cars',
-//     keyword : undefined,
-//     latitude : undefined,
-//     longititude : undefined,
-//  }
-
 const useSearch = () => {
   const [isSearching, setIsSearching] = useState(false);
 
   const search = async (parameters) => {
     // define the request
     const url = `/api/search`;
-
-    // if (parameters.category == undefined) {
-    //   const paramsNew = {
-    //     category: parameters.category,
-    //   };
-    // } else if (parameters.latitude == undefined) {
-    //   const paramsNew = {
-    //     keyword: parameters.keyword,
-    //   };
-    // } else {
-    //   const paramsNew = {
-    //     keyword: parameters.keyword,
-    //     latitude: parameters.latitude,
-    //     longititude: parameters.longititude,
-    //     radius: '10',
-    //   };
-    // }
 
     console.log(parameters);
 
